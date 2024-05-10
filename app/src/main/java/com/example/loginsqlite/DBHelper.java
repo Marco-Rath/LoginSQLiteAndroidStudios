2412d181064020cf5a55e76f4c0b1c0ecc8a05ff
Marco-Rath <rathchipanimarcoantonio@gmail.com>
2024-05-10 15:22:35 -0500

    cuarto commit

--- app/src/main/java/com/example/loginsqlite/DBHelper.java
+++ app/src/main/java/com/example/loginsqlite/DBHelper.java
@@ -36,21 +36,6 @@ public class DBHelper extends SQLiteOpenHelper {
                 "    FOREIGN KEY(user_id) REFERENCES usuarios(user_id)\n" +
                 ");");
 
-        db.execSQL("CREATE TRIGGER AddAmountToUserBalance\n" +
-                "AFTER UPDATE OF Saldo ON usuarios\n" +
-                "FOR EACH ROW\n" +
-                "BEGIN\n" +
-                "    INSERT INTO transactions (user_id, Type, Amount, Date)\n" +
-                "    VALUES (NEW.user_id, 'Depósito', NEW.Saldo, datetime('now', 'localtime'));\n" +
-                "END;\n");
-
-        db.execSQL("CREATE TRIGGER WithdrawAmountFromUserBalance\n" +
-                "AFTER UPDATE OF Saldo ON usuarios\n" +
-                "FOR EACH ROW\n" +
-                "BEGIN\n" +
-                "    INSERT INTO transactions (user_id, Type, Amount, Date)\n" +
-                "    VALUES (NEW.user_id, 'Retiro', NEW.Saldo, datetime('now', 'localtime'));\n" +
-                "END;\n");
 
 
     }
@@ -92,16 +77,16 @@ public class DBHelper extends SQLiteOpenHelper {
         Cursor cursor = db.rawQuery("SELECT * FROM usuarios WHERE (Username=? OR Email=?) AND Password=?", new String[]{Username, Username, Password});
 
         if (cursor.getCount() > 0) {
-            cursor.moveToFirst(); // Mover el cursor al primer registro
+            cursor.moveToFirst(); // Asegúrate de mover el cursor al primer registro
             int nameIndex = cursor.getColumnIndex("Name"); // Obtener el índice de la columna "Name"
             int idIndex = cursor.getColumnIndex("user_id");
             if (nameIndex!= -1 && idIndex!= -1) { // Verificar que el índice no sea -1
                 String name = cursor.getString(nameIndex); // Obtener el nombre del usuario
-                int userId = cursor.getInt(idIndex);//Obtener el id del usuario
+                int userId = cursor.getInt(idIndex); // Obtener el id del usuario
                 cursor.close(); // Cerrar el cursor aquí
                 return name + "," + userId; // Devolver el nombre del usuario
             } else {
-                // Manejar el caso en que la columna "Name" no existe
+                // Manejar el caso en que la columna "Name" o "user_id" no existan
                 return null;
             }
         } else {
@@ -116,15 +101,87 @@ public class DBHelper extends SQLiteOpenHelper {
         db.update("usuarios", values, "user_id=?", new String[]{String.valueOf(userId)});
         db.close();
     }
-    public void withdrawAmountFromUserBalance(int userId, double amount) {
+    public void depositAmount(int userId, double amount) {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put("Saldo", amount);
-        db.update("usuarios", values, "user_id=?", new String[]{String.valueOf(userId)});
+        // Obtener el saldo actual del usuario
+        Cursor cursor = db.rawQuery("SELECT Saldo FROM usuarios WHERE user_id=?", new String[]{String.valueOf(userId)});
+        if (cursor.moveToFirst()) {
+            int saldoIndex = cursor.getColumnIndex("Saldo");
+            if (saldoIndex!= -1) {
+                double currentBalance = cursor.getDouble(saldoIndex);
+                // Sumar el monto del saldo actual
+                double newBalance = currentBalance + amount;
+                ContentValues values = new ContentValues();
+                values.put("Saldo", newBalance);
+                db.update("usuarios", values, "user_id=?", new String[]{String.valueOf(userId)});
+
+                // Insertar la transacción de depósito en la tabla transactions
+                ContentValues transactionValues = new ContentValues();
+                transactionValues.put("user_id", userId);
+                transactionValues.put("Type", "Depósito");
+                transactionValues.put("Amount", amount);
+                long result = db.insert("transactions", null, transactionValues);
+                if (result == -1) {
+                    // Manejar el caso de error
+                } else {
+                    // Transacción insertada correctamente
+                }
+            } else {
+                // Manejar el caso en que la columna "Saldo" no se encuentra
+                throw new IllegalArgumentException("La columna 'Saldo' no se encuentra en la tabla 'usuarios'");
+            }
+        }
         db.close();
     }
 
-    /*public Cursor getAllTransactions() {
+
+
+
+    public void withdrawAmountFromUserBalance(int userId, double amount) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        // Obtener el saldo actual del usuario
+        Cursor cursor = db.rawQuery("SELECT Saldo FROM usuarios WHERE user_id=?", new String[]{String.valueOf(userId)});
+        if (cursor.moveToFirst()) {
+            int saldoIndex = cursor.getColumnIndex("Saldo");
+            if (saldoIndex!= -1) {
+                double currentBalance = cursor.getDouble(saldoIndex);
+                // Restar el monto del saldo actual
+                double newBalance = currentBalance - amount;
+                ContentValues values = new ContentValues();
+                values.put("Saldo", newBalance);
+                db.update("usuarios", values, "user_id=?", new String[]{String.valueOf(userId)});
+            } else {
+                // Manejar el caso en que la columna "Saldo" no se encuentra
+                throw new IllegalArgumentException("La columna 'Saldo' no se encuentra en la tabla 'usuarios'");
+            }
+        }
+        db.close();
+    }
+
+
+    public void withdrawAmount(int userId, double amount) {
+        // Retirar el monto del saldo del usuario
+        withdrawAmountFromUserBalance(userId, amount);
+
+        // Insertar la transacción de retiro en la tabla transactions
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("user_id", userId);
+        values.put("Type", "Retiro");
+        values.put("Amount", amount);
+        // No necesitamos especificar la fecha, ya que el campo Date tiene un valor predeterminado
+        long result = db.insert("transactions", null, values);
+        if (result == -1) {
+            // Manejar el caso de error
+        } else {
+            // Transacción insertada correctamente
+        }
+        db.close();
+    }
+
+
+
+    public Cursor getAllTransactions() {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM transactions", null);
         return cursor;
@@ -132,13 +189,73 @@ public class DBHelper extends SQLiteOpenHelper {
     public String getUserNameById(String userId) {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT Name FROM usuarios WHERE user_id=?", new String[]{userId});
-        if (cursor.moveToFirst()) {
-            return cursor.getString(cursor.getColumnIndex("Name"));
+        if (cursor.moveToFirst()) { // Asegúrate de mover el cursor al primer registro
+            int nameIndex = cursor.getColumnIndex("Name"); // Obtener el índice de la columna "Name"
+            if (nameIndex!= -1) { // Verificar que el índice no sea -1
+                String name = cursor.getString(nameIndex); // Obtener el nombre del usuario
+                cursor.close(); // Cerrar el cursor aquí
+                return name; // Devolver el nombre del usuario
+            } else {
+                // Manejar el caso en que la columna "Name" no exista
+                return null;
+            }
+        } else {
+            return null; // Devolver null si no se encuentra el usuario
         }
-        return null;
     }
 
-     */
+    public double getUserBalance(int userId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT Saldo FROM usuarios WHERE user_id=?", new String[]{String.valueOf(userId)});
+        if (cursor.moveToFirst()) {
+            int saldoIndex = cursor.getColumnIndex("Saldo");
+            if (saldoIndex!= -1) {
+                return cursor.getDouble(saldoIndex);
+            }
+        }
+        return 0; // Devuelve 0 si no se encuentra el usuario o si el saldo no está disponible
+    }
+    public double getCurrentUserBalance(int userId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT Saldo FROM usuarios WHERE user_id=?", new String[]{String.valueOf(userId)});
+        if (cursor.moveToFirst()) {
+            int saldoIndex = cursor.getColumnIndex("Saldo");
+            if (saldoIndex!= -1) {
+                return cursor.getDouble(saldoIndex);
+            }
+        }
+        return 0; // Devuelve 0 si no se encuentra el usuario o si el saldo no está disponible
+    }
+
+
+    public String buscarTransaccionesPorFechaYUsuario(String fechaSeleccionada, int userId) {
+        String query = "SELECT * FROM transactions INNER JOIN usuarios ON transactions.user_id = usuarios.user_id WHERE strftime('%Y-%m-%d', transactions.Date) =? AND usuarios.user_id =?";
+        Cursor cursor = this.getReadableDatabase().rawQuery(query, new String[]{fechaSeleccionada, String.valueOf(userId)});
+        StringBuilder resultado = new StringBuilder();
+        if (cursor.moveToFirst()) {
+            String tableFormat = "%-10s %-10s %-10s %-10s %-10s\n";
+            resultado.append(String.format(tableFormat, "Tipo", "Monto", "Fecha", "Usuario", "ID"));
+            while (cursor.moveToNext()) {
+                int typeIndex = cursor.getColumnIndex("Type");
+                int amountIndex = cursor.getColumnIndex("Amount");
+                int dateIndex = cursor.getColumnIndex("Date");
+                int userIdIndex = cursor.getColumnIndex("user_id");
+                if (typeIndex!= -1 && amountIndex!= -1 && dateIndex!= -1 && userIdIndex!= -1) {
+                    String type = cursor.getString(typeIndex);
+                    double amount = cursor.getDouble(amountIndex);
+                    String date = cursor.getString(dateIndex);
+                    String user_idFromCursor = cursor.getString(userIdIndex);
+                    String userName = this.getUserNameById(user_idFromCursor);
+                    String lineFormat = "%-10s %-10s %-10s %-10s %-10s\n";
+                    resultado.append(String.format(lineFormat, type, amount, date, userName, user_idFromCursor));
+                }
+            }
+        }
+        cursor.close();
+        return resultado.toString();
+    }
+
+
 
 
 
--- app/src/main/java/com/example/loginsqlite/HomeActivity.java
+++ app/src/main/java/com/example/loginsqlite/HomeActivity.java
@@ -20,11 +20,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class HomeActivity extends AppCompatActivity {
     TextView etiNombre;
     Spinner comboTransacciones;
 
-    Button btnBuscarFecha,btnHistorial,btnAceptar;
+    Button btnBuscarFecha,btnHistorial,btnAceptar,btnSaldo,btnBuscar;
     EditText cajaId,cajaMonto,cajaFecha;
 
 
@@ -33,19 +40,23 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         etiNombre = findViewById(R.id.etiNombre);
+        //find component for buttom
+        btnBuscarFecha = findViewById(R.id.btnBuscarFecha);
+        btnHistorial = findViewById(R.id.btnHistorial);
+        btnAceptar = findViewById(R.id.btnAceptar);
+        btnSaldo=findViewById(R.id.btnSaldo);
+        btnBuscar=findViewById(R.id.btnBuscar);
+        //find component for Spinner
+        comboTransacciones = findViewById(R.id.comboTransacciones);
 
-        btnBuscarFecha=findViewById(R.id.btnBuscarFecha);
-        btnHistorial=findViewById(R.id.btnHistorial);
-        btnAceptar=findViewById(R.id.btnAceptar);
-        comboTransacciones=findViewById(R.id.comboTransacciones);
-
-        cajaId=findViewById(R.id.cajaId);
-        cajaMonto=findViewById(R.id.cajaMonto);
-        cajaFecha=findViewById(R.id.cajaFecha);
+        //find component for EditText
+        cajaId = findViewById(R.id.cajaId);
+        cajaMonto = findViewById(R.id.cajaMonto);
+        cajaFecha = findViewById(R.id.cajaFecha);
 
         Intent intent = getIntent();
         String userName = intent.getStringExtra("username");
-        int userId = intent.getIntExtra("userId",-1);
+        int userId = intent.getIntExtra("userId", -1);
 
         if (userName != null) {
             etiNombre.setText(userName);
@@ -80,26 +91,81 @@ public class HomeActivity extends AppCompatActivity {
             Toast.makeText(HomeActivity.this, message, Toast.LENGTH_SHORT).show();
         }
     });
-       /* btnHistorial.setOnClickListener(new View.OnClickListener() {
+        btnHistorial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DBHelper dbHelper = new DBHelper(HomeActivity.this);
-                Cursor cursor = dbHelper.getAllTransactions();
-                StringBuilder transactionHistory = new StringBuilder();
-                while (cursor.moveToNext()) {
-                    String type = cursor.getString(cursor.getColumnIndex("Type"));
-                    double amount = cursor.getDouble(cursor.getColumnIndex("Amount"));
-                    String date = cursor.getString(cursor.getColumnIndex("Date"));
-                    String userId = cursor.getString(cursor.getColumnIndex("user_id"));
-                    // Aquí puedes obtener el nombre del usuario usando el userId
-                    String userName = dbHelper.getUserNameById(userId);
-                    transactionHistory.append("Tipo: ").append(type).append(", Monto: ").append(amount).append(", Fecha: ").append(date).append(", Usuario: ").append(userName).append("\n");
+                String transactionHistory = getTransactionHistory();
+                if (!transactionHistory.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+                    builder.setTitle("Historial de Transacciones");
+                    builder.setMessage(transactionHistory);
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Código para ejecutar cuando se presiona el botón OK
+                        }
+                    });
+                    builder.show();
+                } else {
+                    Toast.makeText(HomeActivity.this, "No hay transacciones disponibles.", Toast.LENGTH_SHORT).show();
+                }
             }
-                cursor.close();
-                // Crear y mostrar el AlertDialog
+        });
+
+    btnSaldo.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            DBHelper dbHelper = new DBHelper(HomeActivity.this);
+            int userId = Integer.parseInt(cajaId.getText().toString());
+            double saldo = dbHelper.getUserBalance(userId);
+            String saldoStr = String.format("%.2f", saldo); // Formatea el saldo con dos decimales
+            AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+            builder.setTitle("Saldo");
+            builder.setMessage("Tu saldo es: " + saldoStr);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Código para ejecutar cuando se presiona el botón OK
+                }
+            });
+            builder.show();
+        }
+    });
+
+        btnBuscarFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+                builder.setTitleText("Selecciona una fecha");
+                final MaterialDatePicker<Long> materialDatePicker = builder.build();
+                materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+                    @Override
+                    public void onPositiveButtonClick(Long aLong) {
+                        String fechaSeleccionada = materialDatePicker.getHeaderText();
+                        // Formatear la fecha a YYYY-MM-DD
+                        SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                        String fechaFormateada = formatoFecha.format(aLong);
+                        // Mostrar la fecha formateada en el EditText
+                        cajaFecha.setText(fechaFormateada);
+
+                    }
+                });
+                materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+            }
+        });
+
+btnBuscar.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        String fechaSeleccionada = cajaFecha.getText().toString();
+        int userId = Integer.parseInt(cajaId.getText().toString());
+        if (!fechaSeleccionada.isEmpty() && userId!= -1) {
+            DBHelper dbHelper = new DBHelper(HomeActivity.this);
+            String resultado = dbHelper.buscarTransaccionesPorFechaYUsuario(fechaSeleccionada, userId);
+            if (!resultado.isEmpty()) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
-                builder.setTitle("Historial de Transacciones");
-                builder.setMessage(transactionHistory.toString());
+                builder.setTitle("Transacciones por Fecha y Usuario");
+                builder.setMessage(resultado);
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -107,8 +173,14 @@ public class HomeActivity extends AppCompatActivity {
                     }
                 });
                 builder.show();
-        });
-*/
+            } else {
+                Toast.makeText(HomeActivity.this, "No hay transacciones disponibles para la fecha y usuario seleccionados.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(HomeActivity.this, "Por favor, seleccione una fecha y un usuario válido.", Toast.LENGTH_SHORT).show();
+        }
+    }
+});
 
 
     }
@@ -121,17 +193,63 @@ public class HomeActivity extends AppCompatActivity {
         String message = "";
         try {
             if (type.equals("Depósito")) {
-                dbHelper.addAmountToUserBalance(userId, amount);
+                dbHelper.depositAmount(userId, amount);
                 message = "Depósito Exitoso";
             } else if (type.equals("Retiro")) {
-                dbHelper.withdrawAmountFromUserBalance(userId, amount);
-                message = "Retiro Exitoso";
+                double currentBalance = dbHelper.getCurrentUserBalance(userId);
+                if (amount > currentBalance) {
+                    message = "Advertencia: Su saldo es Insuficiente ";
+                } else {
+                    dbHelper.withdrawAmount(userId, amount);
+                    message = "Retiro Exitoso";
+                }
             }
+        } catch (NumberFormatException e) {
+            message = "Monto inválido. Por favor, ingrese un número.";
         } catch (Exception e) {
-            // Manejar la excepción, por ejemplo, mostrando un mensaje de error
             message = "Error al realizar la operación";
         }
+
+
         return message;
     }
+
+
+    public String getTransactionHistory() {
+        DBHelper dbHelper = new DBHelper(this);
+        Cursor cursor = dbHelper.getAllTransactions();
+        StringBuilder transactionHistory = new StringBuilder();
+        // Definir el formato de la tabla
+        String tableFormat = "%-10s %-10s %-10s %-10s %-10s\n";
+        // Agregar el encabezado de la tabla
+        transactionHistory.append(String.format(tableFormat, "Tipo", "Monto", "Fecha", "Usuario", "ID"));
+        while (cursor.moveToNext()) {
+            int typeIndex = cursor.getColumnIndex("Type");
+            int amountIndex = cursor.getColumnIndex("Amount");
+            int dateIndex = cursor.getColumnIndex("Date");
+            int userIdIndex = cursor.getColumnIndex("user_id");
+            if (typeIndex!= -1 && amountIndex!= -1 && dateIndex!= -1 && userIdIndex!= -1) {
+                String type = cursor.getString(typeIndex);
+                double amount = cursor.getDouble(amountIndex);
+                String date = cursor.getString(dateIndex);
+                String userId = cursor.getString(userIdIndex);
+                String lineFormat = "%-10s %-10s %-10s %-10s %-10s\n";
+                // Obtener el nombre del usuario usando el userId
+                String userName = dbHelper.getUserNameById(userId);
+
+                // Formatear la línea de la transacción
+                transactionHistory.append(String.format(tableFormat, type, amount, date, userName, userId));
+            }
+        }
+        cursor.close();
+        return transactionHistory.toString();
+    }
+
+
+
+
+
+
+
 
 }
